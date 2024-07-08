73464d68d8356c7ae89495ee43e5d00d9f7ee8a8
sdh9606 <daehun47@gmail.com>
2024-07-08 13:16:50 +0900

    백준 1008 문제

---
+++ src/BackJoon/Ex5.java
@@ -0,0 +1,15 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+
+        System.out.println(A / B);
+    }
+}
