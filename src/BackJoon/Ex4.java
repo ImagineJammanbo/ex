e39fe951b634d28d243faf791d2e59258313980a
sdh9606 <daehun47@gmail.com>
2024-07-08 13:05:03 +0900

    백준 10998 문제

---
+++ src/BackJoon/Ex4.java
@@ -0,0 +1,16 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(A * B);
+
+    }
+}
